52b9c20e4054d9f727e8ee5295f8fa446be2d589
Dell <minhtet.aung@my.jcu.edu.au>
2022-11-27 11:05:47 +0800

    get user input and store it in a list then display the elements in it. Then check for the user's input if it is present in the list

---
+++ prac_03/list_exercises.py
@@ -0,0 +1,18 @@
+numbers =[]
+k = 5
+for i in range (k):
+    user_number = int(input("Number: "))
+    numbers.append(user_number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers)/k}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name = input("Enter the valid name: ")
+if name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
