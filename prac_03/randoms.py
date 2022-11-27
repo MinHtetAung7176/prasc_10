f4d3f0b6ff1877171c85b693b5bc340cefb2d85b
Dell <minhtet.aung@my.jcu.edu.au>
2022-11-27 13:03:23 +0800

    random function exercise

---
+++ prac_03/randoms.py
@@ -0,0 +1,9 @@
+import random
+
+print(random.randint(5, 20))  # line 1 #smallest=6 #largest=12
+print(random.randrange(3, 10, 2))  # line 2 #smallest=3 #largest=9
+print(random.uniform(2.5, 5.5))  # line 3 #smallest=3.671838454450549  #largest=4.608424573112946
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1,100)) # result 95
+
